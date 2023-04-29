3ed84c6b1f668198ae6498e6dc713938c673751a
Fire-orel <78948895+Fire-orel@users.noreply.github.com>
2023-04-29 13:06:15 +0900

    main

---
+++ 2/Form1.Designer.cs
@@ -0,0 +1,380 @@
+﻿namespace _2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            tabControl1 = new TabControl();
+            tabPage1 = new TabPage();
+            dataGridView1 = new DataGridView();
+            update_button = new Button();
+            groupBox1 = new GroupBox();
+            delete_landing_button = new Button();
+            edit_landing_button = new Button();
+            add_landing_button = new Button();
+            tabPage2 = new TabPage();
+            dataGridView2 = new DataGridView();
+            update_button_knigi = new Button();
+            groupBox2 = new GroupBox();
+            delete_landing2_button = new Button();
+            edit_landing2_button = new Button();
+            add_landing2_button = new Button();
+            tabPage3 = new TabPage();
+            dataGridView3 = new DataGridView();
+            update_button_chitateli = new Button();
+            groupBox3 = new GroupBox();
+            delete_landing3_button = new Button();
+            edit_landing3_button = new Button();
+            add_landing3_button = new Button();
+            tabControl1.SuspendLayout();
+            tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            groupBox1.SuspendLayout();
+            tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView2).BeginInit();
+            groupBox2.SuspendLayout();
+            tabPage3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView3).BeginInit();
+            groupBox3.SuspendLayout();
+            SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            tabControl1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            tabControl1.Controls.Add(tabPage1);
+            tabControl1.Controls.Add(tabPage2);
+            tabControl1.Controls.Add(tabPage3);
+            tabControl1.Location = new Point(0, 0);
+            tabControl1.Margin = new Padding(3, 2, 3, 2);
+            tabControl1.Name = "tabControl1";
+            tabControl1.SelectedIndex = 0;
+            tabControl1.Size = new Size(830, 430);
+            tabControl1.TabIndex = 0;
+            tabControl1.SelectedIndexChanged += tabControl1_SelectedIndexChanged_1;
+            // 
+            // tabPage1
+            // 
+            tabPage1.Controls.Add(dataGridView1);
+            tabPage1.Controls.Add(update_button);
+            tabPage1.Controls.Add(groupBox1);
+            tabPage1.Location = new Point(4, 24);
+            tabPage1.Margin = new Padding(3, 2, 3, 2);
+            tabPage1.Name = "tabPage1";
+            tabPage1.Padding = new Padding(3, 2, 3, 2);
+            tabPage1.Size = new Size(822, 402);
+            tabPage1.TabIndex = 0;
+            tabPage1.Text = "Выдача";
+            tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.AllowUserToDeleteRows = false;
+            dataGridView1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(3, 61);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.ReadOnly = true;
+            dataGridView1.RowTemplate.Height = 25;
+            dataGridView1.Size = new Size(813, 338);
+            dataGridView1.TabIndex = 2;
+            dataGridView1.CellContentClick += dataGridView1_CellContentClick_1;
+            // 
+            // update_button
+            // 
+            update_button.Location = new Point(284, 24);
+            update_button.Margin = new Padding(3, 2, 3, 2);
+            update_button.Name = "update_button";
+            update_button.Size = new Size(82, 22);
+            update_button.TabIndex = 1;
+            update_button.Text = "Обновить";
+            update_button.UseVisualStyleBackColor = true;
+            update_button.Click += update_button_Click_1;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(delete_landing_button);
+            groupBox1.Controls.Add(edit_landing_button);
+            groupBox1.Controls.Add(add_landing_button);
+            groupBox1.Location = new Point(7, 4);
+            groupBox1.Margin = new Padding(3, 2, 3, 2);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Padding = new Padding(3, 2, 3, 2);
+            groupBox1.Size = new Size(272, 52);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Управление";
+            // 
+            // delete_landing_button
+            // 
+            delete_landing_button.Location = new Point(182, 20);
+            delete_landing_button.Margin = new Padding(3, 2, 3, 2);
+            delete_landing_button.Name = "delete_landing_button";
+            delete_landing_button.Size = new Size(82, 22);
+            delete_landing_button.TabIndex = 2;
+            delete_landing_button.Text = "Удалить";
+            delete_landing_button.UseVisualStyleBackColor = true;
+            // 
+            // edit_landing_button
+            // 
+            edit_landing_button.Location = new Point(93, 20);
+            edit_landing_button.Margin = new Padding(3, 2, 3, 2);
+            edit_landing_button.Name = "edit_landing_button";
+            edit_landing_button.Size = new Size(82, 22);
+            edit_landing_button.TabIndex = 1;
+            edit_landing_button.Text = "Изменит";
+            edit_landing_button.UseVisualStyleBackColor = true;
+            // 
+            // add_landing_button
+            // 
+            add_landing_button.Location = new Point(5, 20);
+            add_landing_button.Margin = new Padding(3, 2, 3, 2);
+            add_landing_button.Name = "add_landing_button";
+            add_landing_button.Size = new Size(82, 22);
+            add_landing_button.TabIndex = 0;
+            add_landing_button.Text = "Добавить";
+            add_landing_button.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            tabPage2.Controls.Add(dataGridView2);
+            tabPage2.Controls.Add(update_button_knigi);
+            tabPage2.Controls.Add(groupBox2);
+            tabPage2.Location = new Point(4, 24);
+            tabPage2.Margin = new Padding(3, 2, 3, 2);
+            tabPage2.Name = "tabPage2";
+            tabPage2.Padding = new Padding(3, 2, 3, 2);
+            tabPage2.Size = new Size(822, 402);
+            tabPage2.TabIndex = 1;
+            tabPage2.Text = "Книги";
+            tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView2
+            // 
+            dataGridView2.AllowUserToAddRows = false;
+            dataGridView2.AllowUserToDeleteRows = false;
+            dataGridView2.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            dataGridView2.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridView2.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView2.Location = new Point(5, 62);
+            dataGridView2.Margin = new Padding(3, 2, 3, 2);
+            dataGridView2.Name = "dataGridView2";
+            dataGridView2.ReadOnly = true;
+            dataGridView2.RowHeadersWidth = 51;
+            dataGridView2.RowTemplate.Height = 29;
+            dataGridView2.Size = new Size(814, 338);
+            dataGridView2.TabIndex = 3;
+            // 
+            // update_button_knigi
+            // 
+            update_button_knigi.Location = new Point(284, 24);
+            update_button_knigi.Margin = new Padding(3, 2, 3, 2);
+            update_button_knigi.Name = "update_button_knigi";
+            update_button_knigi.Size = new Size(82, 22);
+            update_button_knigi.TabIndex = 2;
+            update_button_knigi.Text = "Обновить";
+            update_button_knigi.UseVisualStyleBackColor = true;
+            update_button_knigi.Click += update_button_knigi_Click_1;
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(delete_landing2_button);
+            groupBox2.Controls.Add(edit_landing2_button);
+            groupBox2.Controls.Add(add_landing2_button);
+            groupBox2.Location = new Point(7, 4);
+            groupBox2.Margin = new Padding(3, 2, 3, 2);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Padding = new Padding(3, 2, 3, 2);
+            groupBox2.Size = new Size(272, 52);
+            groupBox2.TabIndex = 1;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "Управление";
+            // 
+            // delete_landing2_button
+            // 
+            delete_landing2_button.Location = new Point(182, 20);
+            delete_landing2_button.Margin = new Padding(3, 2, 3, 2);
+            delete_landing2_button.Name = "delete_landing2_button";
+            delete_landing2_button.Size = new Size(82, 22);
+            delete_landing2_button.TabIndex = 2;
+            delete_landing2_button.Text = "Удалить";
+            delete_landing2_button.UseVisualStyleBackColor = true;
+            // 
+            // edit_landing2_button
+            // 
+            edit_landing2_button.Location = new Point(93, 20);
+            edit_landing2_button.Margin = new Padding(3, 2, 3, 2);
+            edit_landing2_button.Name = "edit_landing2_button";
+            edit_landing2_button.Size = new Size(82, 22);
+            edit_landing2_button.TabIndex = 1;
+            edit_landing2_button.Text = "Изменит";
+            edit_landing2_button.UseVisualStyleBackColor = true;
+            // 
+            // add_landing2_button
+            // 
+            add_landing2_button.Location = new Point(5, 20);
+            add_landing2_button.Margin = new Padding(3, 2, 3, 2);
+            add_landing2_button.Name = "add_landing2_button";
+            add_landing2_button.Size = new Size(82, 22);
+            add_landing2_button.TabIndex = 0;
+            add_landing2_button.Text = "Добавить";
+            add_landing2_button.UseVisualStyleBackColor = true;
+            // 
+            // tabPage3
+            // 
+            tabPage3.Controls.Add(dataGridView3);
+            tabPage3.Controls.Add(update_button_chitateli);
+            tabPage3.Controls.Add(groupBox3);
+            tabPage3.Location = new Point(4, 24);
+            tabPage3.Margin = new Padding(3, 2, 3, 2);
+            tabPage3.Name = "tabPage3";
+            tabPage3.Size = new Size(822, 402);
+            tabPage3.TabIndex = 2;
+            tabPage3.Text = "Читатели";
+            tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView3
+            // 
+            dataGridView3.AllowUserToAddRows = false;
+            dataGridView3.AllowUserToDeleteRows = false;
+            dataGridView3.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            dataGridView3.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridView3.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView3.Location = new Point(3, 59);
+            dataGridView3.Margin = new Padding(3, 2, 3, 2);
+            dataGridView3.Name = "dataGridView3";
+            dataGridView3.ReadOnly = true;
+            dataGridView3.RowHeadersWidth = 51;
+            dataGridView3.RowTemplate.Height = 29;
+            dataGridView3.Size = new Size(814, 338);
+            dataGridView3.TabIndex = 4;
+            // 
+            // update_button_chitateli
+            // 
+            update_button_chitateli.Location = new Point(284, 22);
+            update_button_chitateli.Margin = new Padding(3, 2, 3, 2);
+            update_button_chitateli.Name = "update_button_chitateli";
+            update_button_chitateli.Size = new Size(82, 22);
+            update_button_chitateli.TabIndex = 3;
+            update_button_chitateli.Text = "Обновить";
+            update_button_chitateli.UseVisualStyleBackColor = true;
+            update_button_chitateli.Click += update_button_chitateli_Click_1;
+            // 
+            // groupBox3
+            // 
+            groupBox3.Controls.Add(delete_landing3_button);
+            groupBox3.Controls.Add(edit_landing3_button);
+            groupBox3.Controls.Add(add_landing3_button);
+            groupBox3.Location = new Point(7, 2);
+            groupBox3.Margin = new Padding(3, 2, 3, 2);
+            groupBox3.Name = "groupBox3";
+            groupBox3.Padding = new Padding(3, 2, 3, 2);
+            groupBox3.Size = new Size(272, 52);
+            groupBox3.TabIndex = 2;
+            groupBox3.TabStop = false;
+            groupBox3.Text = "Управление";
+            // 
+            // delete_landing3_button
+            // 
+            delete_landing3_button.Location = new Point(182, 20);
+            delete_landing3_button.Margin = new Padding(3, 2, 3, 2);
+            delete_landing3_button.Name = "delete_landing3_button";
+            delete_landing3_button.Size = new Size(82, 22);
+            delete_landing3_button.TabIndex = 2;
+            delete_landing3_button.Text = "Удалить";
+            delete_landing3_button.UseVisualStyleBackColor = true;
+            // 
+            // edit_landing3_button
+            // 
+            edit_landing3_button.Location = new Point(93, 20);
+            edit_landing3_button.Margin = new Padding(3, 2, 3, 2);
+            edit_landing3_button.Name = "edit_landing3_button";
+            edit_landing3_button.Size = new Size(82, 22);
+            edit_landing3_button.TabIndex = 1;
+            edit_landing3_button.Text = "Изменит";
+            edit_landing3_button.UseVisualStyleBackColor = true;
+            // 
+            // add_landing3_button
+            // 
+            add_landing3_button.Location = new Point(5, 20);
+            add_landing3_button.Margin = new Padding(3, 2, 3, 2);
+            add_landing3_button.Name = "add_landing3_button";
+            add_landing3_button.Size = new Size(82, 22);
+            add_landing3_button.TabIndex = 0;
+            add_landing3_button.Text = "Добавить";
+            add_landing3_button.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(830, 430);
+            Controls.Add(tabControl1);
+            Margin = new Padding(3, 2, 3, 2);
+            Name = "Form1";
+            Text = "Библеотека";
+            Load += Form1_Load_1;
+            tabControl1.ResumeLayout(false);
+            tabPage1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            groupBox1.ResumeLayout(false);
+            tabPage2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)dataGridView2).EndInit();
+            groupBox2.ResumeLayout(false);
+            tabPage3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)dataGridView3).EndInit();
+            groupBox3.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private TabControl tabControl1;
+        private TabPage tabPage1;
+        private Button update_button;
+        private GroupBox groupBox1;
+        private Button delete_landing_button;
+        private Button edit_landing_button;
+        private Button add_landing_button;
+        private TabPage tabPage2;
+        private TabPage tabPage3;
+        private DataGridView dataGridView2;
+        private Button update_button_knigi;
+        private GroupBox groupBox2;
+        private Button delete_landing2_button;
+        private Button edit_landing2_button;
+        private Button add_landing2_button;
+        private GroupBox groupBox3;
+        private Button delete_landing3_button;
+        private Button edit_landing3_button;
+        private Button add_landing3_button;
+        private DataGridView dataGridView3;
+        private Button update_button_chitateli;
+        private DataGridView dataGridView1;
+    }
+}
---
+++ 2/Form1.cs
@@ -0,0 +1,101 @@
+using System.Data.SQLite;
+using System.Data;
+
+namespace _2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGridView1_CellContentClick_1(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void update_button_Click_1(object sender, EventArgs e)
+        {
+            SQLiteConnection con = new SQLiteConnection(@"data source=library.db");
+
+            con.Open();
+
+            string query = "SELECT id AS Код,\r\n\r\nbooks.author || \" - \" || books.name AS Книга,\r\n\r\nreaders.last_name || \" \" || readers.first_name || \" \" || readers.patronimyc AS Читатель,\r\n\r\nstrftime('%d.%m.%Y', date_begin) AS Дата_выдачи,\r\n\r\nstrftime('%d.%m.%Y', date_end) AS Дата_возврата\r\n\r\nFROM lending\r\n\r\nINNER JOIN books ON lending.id_book = books.id_books\r\n\r\nINNER JOIN readers ON lending.id_reader = readers.id_reader;";
+
+            SQLiteCommand cmd = new SQLiteCommand(query, con);
+
+            DataTable dt = new DataTable();
+
+            SQLiteDataAdapter adapter = new SQLiteDataAdapter(cmd);
+
+            adapter.Fill(dt);
+
+            dataGridView1.DataSource = dt;
+
+            con.Close();
+        }
+
+        private void update_button_knigi_Click_1(object sender, EventArgs e)
+        {
+            SQLiteConnection con = new SQLiteConnection(@"data source=library.db");
+
+            con.Open();
+
+            string query = "SELECT * FROM books;";
+
+            SQLiteCommand cmd = new SQLiteCommand(query, con);
+
+            DataTable dt = new DataTable();
+
+            SQLiteDataAdapter adapter = new SQLiteDataAdapter(cmd);
+
+            adapter.Fill(dt);
+
+            dataGridView2.DataSource = dt;
+
+            con.Close();
+        }
+
+        private void update_button_chitateli_Click_1(object sender, EventArgs e)
+        {
+            SQLiteConnection con = new SQLiteConnection(@"data source=library.db");
+
+            con.Open();
+
+            string query = "SELECT * FROM readers;";
+
+            SQLiteCommand cmd = new SQLiteCommand(query, con);
+
+            DataTable dt = new DataTable();
+
+            SQLiteDataAdapter adapter = new SQLiteDataAdapter(cmd);
+
+            adapter.Fill(dt);
+
+            dataGridView3.DataSource = dt;
+
+            con.Close();
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            update_button_Click_1(sender, e);
+        }
+
+        private void tabControl1_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+            switch (tabControl1.SelectedIndex.ToString())
+            {
+                case ("0"):
+                    update_button_Click_1(sender, e); break;
+                case ("1"):
+                    update_button_knigi_Click_1(sender, e); break;
+                case ("2"):
+                    update_button_chitateli_Click_1(sender, e); break;
+
+            }
+
+        }
+    }
+}
